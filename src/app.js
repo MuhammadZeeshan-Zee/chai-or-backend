fcb3aa501024832af5eb44522446964d26646bfa
zeeshan <zeeshanarif34101@gmail.com>
2024-11-12 12:45:07 +0500

    book modules added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN })); //request is vailed from which origin
+app.use(cors()); //request is vailed from which origin
 app.use(express.json({ limit: "16kb" })); //to get data in json and also give the data limit
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //to get data from url and also give the data limit
 app.use(express.static("public")); //serve static assets, this data can access everyone
--- src/route/book.route.js
+++ src/route/book.route.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createbookService,getAllBookedOrders,updateOrderStatus } from "../controller/book.controller.js";
+import {
+  createbookService,
+  getAllBookedOrders,
+  updateOrderStatus,
+} from "../controller/book.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { AdminVerify } from "../middleware/adminVerify.middleware.js";
 
 const router = Router();
-router.route("/order").post(verifyJWT,createbookService);
-router.route("/getAllOrders").get(verifyJWT,getAllBookedOrders);
-router.route("/updateOrderStatus/:id").post(verifyJWT,updateOrderStatus);
+router.route("/order").post(createbookService);
+router.route("/getAllOrders").get(verifyJWT, getAllBookedOrders);
+router.route("/updateOrderStatus/:id").post(verifyJWT, updateOrderStatus);
 export default router;
-
